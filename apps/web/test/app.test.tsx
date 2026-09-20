1a10212dc96f8551182192d00f1dc6ba150aab69
Jon Schaffer <nograviti@gmail.com>
2026-09-19 20:15:23 -0400

    fix(repo): resolve test issue

--- apps/web/test/app.test.tsx
+++ apps/web/test/app.test.tsx
@@ -1,16 +1,12 @@
 import { render, screen } from '@testing-library/react'
-import { afterEach, describe, it, vi } from 'vitest'
+import { afterEach, describe, it, vi, expect } from 'vitest'
 
-import { App } from '../src/App'
+import App from '../src/App'
 
 describe('App', () => {
-  afterEach(() => {
-    vi.unstubAllGlobals();
-  });
-
   it('renders the zerograviti application', async () => {
     render(<App />);
 
-    expect(screen.findByText('Get started')).toBeDefined();
+    expect(screen.findByText('Get Started')).toBeDefined();
   });
 })
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,9 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom',
+  },
+});
